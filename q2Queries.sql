e085f550442328b74319fc3f8e387612c3bd9584
Thymen <thymen01@gmail.com>
2019-03-09 17:54:51 +0100

    Updated Query 7,added ordered by

--- q2Queries.sql
+++ q2Queries.sql
@@ -4,5 +4,5 @@ CREATE VIEW active_per_degree(Gender, Amount) as (SELECT s.Gender, count(distinc
 SELECT CAST(fpd.Amount AS FLOAT) / spd.Amount FROM females_per_department as fpd JOIN students_per_department as spd ON spd.Department = fpd.Department WHERE fpd.Department = %1%;
 SELECT 0;
 SELECT 0;
-SELECT gac.DegreeId, s.BirthYearStudent, s.Gender, CAST(sum(Weighted) AS FLOAT) / sum(sumECTS) as avgGPA FROM gpa_active_complete as gac JOIN Students as s ON gac.StudentId = s.StudentId WHERE gac.Complete = 0 GROUP BY CUBE (gac.DegreeId, s.BirthYearStudent, s.Gender);
+SELECT gac.DegreeId, s.BirthYearStudent, s.Gender, CAST(sum(Weighted) AS FLOAT) / sum(sumECTS) as avgGPA FROM gpa_active_complete as gac JOIN Students as s ON gac.StudentId = s.StudentId WHERE gac.Complete = 0 GROUP BY CUBE (gac.DegreeId, s.BirthYearStudent, s.Gender) ORDER BY gac.DegreeId, s.BirthYearStudent, s.Gender;
 SELECT c.CourseName, co.year, co.quartile FROM total_students_and_assistants_per_offer AS tsaapo  JOIN CourseOffers AS co ON co.CourseOfferID = tsaapo.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId ORDER BY tsaapo.CourseOfferId;
