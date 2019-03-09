2d4998d11aefdf9d93f87f2049a66bc39d43d990
Thymen <thymen01@gmail.com>
2019-03-09 15:38:18 +0100

    REmoved Query 1

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,4 +1,4 @@
-SELECT c.CourseName, Grade FROM  all_courses_passed as acp JOIN CourseOffers as co ON co.CourseOfferId = acp.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId WHERE acp.StudentId = %1% AND acp.DegreeId = %2% ORDER BY co.Year, co.Quartile, co.CourseOfferId;
+SELECT 0;
 SELECT DISTINCT StudentId FROM high_gpa_no_fail WHERE GPA > %1% ORDER BY StudentId;
 SELECT 0;
 SELECT CAST(fpd.Amount AS FLOAT) / spd.Amount FROM females_per_department as fpd JOIN students_per_department as spd ON spd.Department = fpd.Department WHERE fpd.Department = %1%;
