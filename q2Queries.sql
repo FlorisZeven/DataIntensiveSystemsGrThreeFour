0c96aca663fff8ec866155e6ea5648e43fd0f717
Thymen <thymen01@gmail.com>
2019-03-09 15:31:33 +0100

    Added

--- q2Queries.sql
+++ q2Queries.sql
@@ -1,7 +1,7 @@
 SELECT c.CourseName, Grade FROM  all_courses_passed as acp JOIN CourseOffers as co ON co.CourseOfferId = acp.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId WHERE acp.StudentId = %1% AND acp.DegreeId = %2% ORDER BY co.Year, co.Quartile, co.CourseOfferId;
 SELECT 0;
 SELECT 0;
-SELECT CAST(fpd.Amount AS FLOAT) / spd.Amount FROM females_per_department as fpd JOIN students_per_department as spd ON spd.Department = fpd.Department WHERE Department = %1%;
+SELECT CAST(fpd.Amount AS FLOAT) / spd.Amount FROM females_per_department as fpd JOIN students_per_department as spd ON spd.Department = fpd.Department WHERE fpd.Department = %1%;
 SELECT 0;
 SELECT 0;
 SELECT 0;
