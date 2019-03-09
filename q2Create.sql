981a4abd317cd776a901967ac35962b5ea579e4c
Thymen <thymen01@gmail.com>
2019-03-09 17:50:43 +0100

    Updated Query 7, naar weighted averages

--- q2Create.sql
+++ q2Create.sql
@@ -61,9 +61,9 @@ CREATE MATERIALIZED VIEW all_courses_passed(StudentId, DegreeId, CourseOfferId, 
 CREATE INDEX acp_studentid_degreeid ON all_courses_passed(StudentId, DegreeId);
 
 
-CREATE MATERIALIZED VIEW gpa_active_complete(StudentId, DegreeId, GPA, complete) AS
+CREATE MATERIALIZED VIEW gpa_active_complete(StudentId, DegreeId, Weighted, sumECTS, GPA, complete) AS
 (
-    SELECT acp.StudentId, acp.DegreeId, cast(sum(Grade*ECTS) AS FLOAT) / sum(ECTS) as GPA, case when sum(ECTS) >=  TotalECTS then 1 else 0 end as complete
+    SELECT acp.StudentId, acp.DegreeId, sum(Grade*ECTS) as Weighted, sum(ECTS) as sumECTS, CAST(sum(Grade*ECTS) AS  FLOAT) / sum(ECTS), case when  sum(ECTS) >=  TotalECTS then 1 else 0 end as complete
       FROM all_courses_passed as acp JOIN Degrees as d ON d.DegreeId = acp.DegreeId
       JOIN CourseOffers as co ON co.CourseOfferId = acp.CourseOfferId
       JOIN Courses as c ON c.CourseId = co.CourseId
