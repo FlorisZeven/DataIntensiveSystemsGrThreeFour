404dfad43c59a47982e309a3a4c3dda5246b119c
FlorisZeven <floris_zeven@hotmail.com>
2019-03-12 22:59:58 +0100

    Q6 uit for testing

--- q2Queries.sql
+++ q2Queries.sql
@@ -3,6 +3,6 @@ SELECT DISTINCT StudentId FROM high_gpa_no_fail WHERE HIGHGPA > %1% ORDER BY Stu
 SELECT DegreeId, CAST(FemaleAmount AS FLOAT) / Amount FROM active_per_degree as apd ORDER BY DegreeId;
 SELECT CAST(fpd.Amount AS FLOAT) / spd.Amount FROM females_per_department as fpd JOIN students_per_department as spd ON spd.Department = fpd.Department WHERE fpd.Department = %1%;
 SELECT 0;
-SELECT DISTINCT StudentId, COUNT(StudentID) FROM ExcellentStudents2018Q1 GROUP BY StudentId HAVING COUNT(StudentId) >= 3;
+SELECT 0;
 SELECT gac.DegreeId, s.BirthYearStudent, s.Gender, CAST(sum(Weighted) AS FLOAT) / sum(sumECTS) as avgGPA FROM gpa_active_complete as gac JOIN Students as s ON gac.StudentId = s.StudentId WHERE gac.Complete = 0 GROUP BY CUBE (gac.DegreeId, s.BirthYearStudent, s.Gender) ORDER BY gac.DegreeId, s.BirthYearStudent, s.Gender;
 SELECT DISTINCT c.CourseName, co.year, co.quartile FROM total_students_and_assistants_per_offer AS tsaapo  JOIN CourseOffers AS co ON co.CourseOfferID = tsaapo.CourseOfferId JOIN Courses as c ON c.CourseId = co.CourseId ORDER BY tsaapo.CourseOfferId;
